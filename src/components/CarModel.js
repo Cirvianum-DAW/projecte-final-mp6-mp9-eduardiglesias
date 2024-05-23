af6d54bd77666ef13daf0b10604016cde40955b9
PABLO ROS PROFE <pros7@xtec.cat>
2024-05-23 19:50:27 +0200

    paquet parcel afegir imatges estàtiques

--- src/components/CarModel.js
+++ src/components/CarModel.js
@@ -5,17 +5,14 @@ class CarModelComponent {
     this.description = data.description;
   }
 
-
   render() {
-
-    // const images = require.context("../assets", true);
     const itemElement = document.createElement("div");
     itemElement.className =
       "max-w-sm bg-white border rounded-lg overflow-hidden m-4 shadow-md";
 
     itemElement.innerHTML = `
         <h2 class="text-xl font-bold p-4">${this.name}</h2>
-          <img src="${imagePath}" class="w-full h-48 object-cover">
+          <img src="${this.image}.jpg" class="w-full h-48 object-cover">
         <p class="p-4 hidden md:block">${this.description}</p>
       `;
 
